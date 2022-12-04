4c3964ddd9a4a611d32ab6e6735cc2cd85da5c70
arfin foysal <foysal@bacbon.com>
2022-12-04 17:34:33 +0600

    per

--- src/views/documents/DocumentCategoryCard.js
+++ src/views/documents/DocumentCategoryCard.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function DocumentCategoryCard() {
+const DocumentCategoryCard = () => {
   return (
     <div>DocumentCategoryCard</div>
   )
 }
+
+export default DocumentCategoryCard
