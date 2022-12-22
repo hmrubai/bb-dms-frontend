3d0d1feeaacd62976fa89bedb0b3d4a4ef97c599
arfin foysal <foysal@bacbon.com>
2022-12-22 17:36:55 +0600

    permision edit

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1,4 +1,10 @@
-export function checkObjectInArray(obj, array, id,permission_id) {
+
+//Edit User Permission
+//All Permission And User Permission Checking
+//array == User Permission  ->>permission_id
+//obj == All permission ->>id
+
+export function checkObjectInArray(obj, array, id, permission_id) {
   for (let i = 0; i < array.length; i++) {
     if (array[i][permission_id] === obj[id]) {
       return true;
